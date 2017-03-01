2c47e39a3ac77e0256f9314c9524aa210d9f1ffc
Jean Davilus <jsd6@njit.edu>
2017-03-01 04:01:53 -0500

    computer selection is done

---
+++ assets/javascript/game.js
@@ -0,0 +1,124 @@
+
+var geography=["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua and Barbuda","Argentina","Armenia",
+"Australia","Austria","Azerbaijan","Bahrain","Bangladesh","Barbados ","Belarus","Belgium","Belize","Benin","Bhutan",
+"Bolivia","Bosnia and Herzegovina","Botswana ","Brazil","Brunei ","Bulgaria","Burkina Faso ","Burma","Burundi ",
+"Cambodia","Cameroon","Canada","Cape Verde","Central African Republic","Chad","Chile","China","Colombia","Comoros",
+"Costa Rica","Cote d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Democratic Republic of the Congo","Denmark",
+"Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea",
+"Estonia","Ethiopia","Federated States of Micronesia","Fiji ","Finland","Gabon","Georgia","Germany","Ghana ","Greece",
+"Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia",
+"Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kuwait",
+"Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia",
+"Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Moldova",
+"Monaco","Mongolia","Morocco","Mozambique","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua",
+"Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru",
+"Philippines","Poland","Portugal","Qatar","Republic of the Congo","Romania","Russia","Rwanda","Saint Kitts and Nevis",
+"Saint Lucia","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal",
+"Serbia and Montenegro","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia",
+"South Africa","South Korea","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan",
+"Tajikistan","Tanzania","Thailand","The Bahamas","The Gambia","Togo","Tonga","Trinidad and Tobago","Tunisia",
+"Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States",
+"Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe","Kabul","Tirane",
+"Algiers","Andorra la Vella","Luanda","Saint John's","Buenos Aires","Yerevan","Canberra","Vienna","Baku","Manama",
+"Dhaka","Bridgetown","Minsk","Brussels","Belmopan","Porto-Novo","Thimphu","La Paz","Sarajevo","Gaborone","Brasilia",
+"Bandar Seri Begawan","Sofia","Ouagadougou","Rangoon","Bujumbura","Phnom Penh","Yaounde","Ottawa","Praia","Bangui",
+"N'Djamena","Santiago","Beijing","Bogota","Moroni","San Jose","Yamoussoukro","Zagreb","Havana","Nicosia","Prague",
+"Kinshasa","Copenhagen","Djibouti","Roseau","Santo Domingo","Dili","Quito","Cairo","San Salvador","Malabo","Asmara",
+"Tallinn","Addis Ababa","Palikir","Suva","Helsinki","Libreville","Tbilisi","Berlin","Accra","Athens","Saint George's",
+"Guatemala City","Conakry","Bissau","Georgetown","Port-au-Prince","Tegucigalpa","Budapest","Reykjavik","New Delhi",
+"Jakarta","Tehran","Baghdad","Dublin","Jerusalem","Rome","Kingston","Tokyo","Amman","Astana","Nairobi","Tarawa","Kuwait City",
+"Bishtek","Vientiane","Riga","Beirut","Maseru","Monrovia","Tripoli","Vaduz","Vilnius","Luxembourg","Skopje","Antananarivo",
+"Lilongwe","Kuala Lumpur","Male","Bamako","Valletta","Majuro","Nouakchott","Port Louis","Mexico City","Chisinau","Monaco",
+"Ulaanbaatar","Rabat","Maputo","Windhoek","Yaren District","Kathmandu","Amsterdam","Wellington","Managua","Niamey",
+"Abuja","Pyongyang","Oslo","Muscat","Islamabad","Koror","Panama City","Port Moresby","Asuncion","Lima","Manila","Warsaw",
+"Lisbon","Doha","Brazzaville","Bucharest","Moscow","Kigali","Basseterre","Castries","Kingstown","Apia","San Marino",
+"Sao Tome","Riyadh","Dakar","Belgrade","Victoria","Freetown","Singapore","Bratislava","Ljubljana","Honiara","Mogadishu",
+"Pretoria","Seoul","Madrid","Colombo","Khartoum","Paramaribo","Mbabana","Stockholm","Bern","Damascus","Taipei","Dushanbe",
+"Dar es Salaam","Bangkok","Nassau","Banjul","Lome","Nuku'alofa","Port-of-Spain","Tunis","Ankara","Ashgabat","Funafuti",
+"Kampala","Kiev","Abu Dhabi","London","Washington D.C.","Montevideo","Tashkent","Port-Vila","Vatican City","Caracas","Hanoi",
+"Sanaa","Lusaka","Harare","Alabama","Montana","Alaska","Nebraska","Arizona","Nevada","Arkansas","New Hampshire",
+"California","New Jersey","Colorado","New Mexico","Connecticut","New York","Delaware","North Carolina","Florida",
+"North Dakota","Georgia","Ohio","Hawaii","Oklahoma","Idaho","Oregon","Illinois","Pennsylvania","Indiana","Rhode Island",
+"Iowa","South Carolina","Kansas","South Dakota","Kentucky","Tennessee","Louisiana","Texas","Maine","Utah","Maryland",
+"Vermont","Massachusetts","Virginia","Michigan","Washington","Minnesota","West Virginia","Mississippi","Wisconsin",
+"Missouri","Wyoming","Montgomery","Helena","Juneau","Lincoln","Phoenix","Carson City","Little Rock","Concord",
+"Sacramento","Trenton","Denver","Santa Fe","Hartford","Albany","Dover","Raleigh","Tallahassee","Bismarck","Atlanta",
+"Columbus","Honolulu","Oklahoma City","Boise","Salem","Springfield","Harrisburg","Indianapolis","Providence",
+"Des Moines","Columbia","Topeka","Pierre","Frankfort","Nashville","Baton Rouge","Austin","Augusta","Salt Lake City",
+"Annapolis","Montpelier","Boston","Richmond","Lansing","Olympia","St. Paul","Charleston","Jackson","Madison",
+"Jefferson City","Cheyenne"];
+
+var sports=[ "croquet","motor boating","polo","tug-of-war","plunge for distance","underwater swimming","obstacle race for swimming",
+"cricket","lacrosse","jeu de paume","racquets","squash","wakeboard","wushu","dancing","bowling","netball","baseball ",
+"softball","karate","roller sport","climbing","surfing","rugby union","archery","badminton","basketball","beach volleyball",
+"boxing","canoe","kayak","climbing","cycling","track","road","mountain","BMX","diving","equestrian","dressage",
+"jumping","eventing","fencing","field hockey","gymnastics","handball","modern pentathlon","roller sport","rowing",
+"rugby 7s","sailing","shooting","soccer","football","swimming","synchronized swimming","table tennis","taekwondo",
+"tennis","track and field","triathlon","volleyball","water polo","weightlifting","wrestling"];
+
+var general=["Pencil stylus","Rock the vote","Cheese Potato","Tomato soup","Ace of Spades","Ipod touch","Clothes Hanger",
+"Wrestling Match","Think Tank","Computer virus","hot Plate","Trash Can","Golden Globe","Lacrosse Stick","Electric Lamp",
+"Green Peas","Peach and Mango","Quesada","Couch potato","vertical Plane","Stretcher","Folding Chair","Pressure Cooker",
+"Hot Soup","Bullet proof","police Siren","Katana","Sushi place","The Notebook","House for sale","Butter Knife",
+"Flat screen TV","Microwave","Boxing Glove","Sexy Black Dress","Red Bandana","MC Hammer","Fire Hydrant","Mini Fridge",
+"Garden Trowel","Google Map","Lotion warmer","Mailbox","Lunch Tray","The Wire","That number","Banjo lessons",
+"Harmonica","DVD cleaner","Xbox slim","Baseball Cap","Jar of Cherries","Vanilla Ice","Bellabeat Leaf","Papaya Tree",
+"AK-47","Marinara Sauce","Trump’s Wig","Go-Kart","Cutlass","Busted Door","Overboard","Chocolate Syrup","Wifi Router",
+"Eggplant","Parasol","Check Please","Microphone","Watch Me Nay nay","Security Camera","Vending Truck","Ski Pole",
+"Taco Bell","Star Wars","Red Beer","Vending Machine","Hair Spray","Crushed Skull","love and Basketball","Kale and broccoli",
+"Pineapple Express","Locker room","Dank Meme","Matchbox","Tin Can","Golf Club","Raspberry","Peter Pan","Rolling Pin",
+"Throw me a Bone","Adult Diapers","Medal of honor","homemade Soap","Diet Soda","Trophy Wife","Cheese burger",
+"Eating hotdog","hot buns","baggie jeans","beanie","skinny jeans","hoodie","vans and buses","cake mix","cookies dough",
+"brownie","American pie","banana split","paper cutter","sandwich maker","organic crackers ","Glass pipe ","cool whip can ",
+"cheese block","macaroni and cheese","duct tape","Rope the cows","Pencil in","Pen refills","File Eraser","Fork lift",
+"Necklace","Wendy’s Restaurant","McDonald’s","Burger King","Chicken Popeyes","Whitney Houston","Toni Braxton",
+"Michael Jackson","Nina Simon","Ray Charles","Bruce Springsteen","Diana Ross","The Temptation","Frank Sinatra",
+"Lady Gaga","Adele","Denzel Washington","Viola Davis","Nelson Mandela","Barack Obama","Martin Luther King",
+"Tristan and Isolde","Romeo and Juliet","John Legend","All of Me","I will always love you","We are the world",
+"When you believe","Will Smith","Jesse Jackson","Elizabeth Warren","Bernie Sanders","Feel the Bern","Yes We can",
+"Mind your own business","Halle Berry","Nia Long","Ice Cube","Ice T","Tu Pac","May the power be with you",
+"Trayvon Martin","I can’t Breathe","Hands up don’t shoot","Charlize Theron","Meet the press","Tim Russert","Dave Chapelle",
+"Chris Rock","Bill Maher","Colin Kaepernick","Mahatma Gandhi","Greg Popovich","Lebron James","Steph Curry","Steve Kerr",
+"Dwayne Wade","Jackie Robinson","Muhammad Ali","Jesse Owens","Rosa Parks","Tim Duncan","Michael Bennett","Stevie Wonder"];
+
+var selection;
+
+function wordSelect(){
+
+var groupArray = [1, 2, 3];
+
+
+var groupSelect= Math.floor(Math.random()*groupArray.length);
+
+if(groupSelect=== 3){
+var selection = general[Math.floor(Math.random()*general.length)];
+}
+
+else if(groupSelect===2){
+	var selection =  sports[Math.floor(Math.random()*sports.length)];
+}
+
+else{
+	var selection = geography[Math.floor(Math.random()*geography.length)];
+}
+
+
+for(var i=0; i<selection.length; i++){
+	//var wordToGuess= selection.replace(selection.charAt(i), "-");
+	var wordToGuess= selection.replace(/./gi, "-");
+
+}
+
+var remainingGuess= selection.length;
+var score= 0;
+var countdown=selection.length;
+
+console.log(wordToGuess);
+document.getElementById('output').innerHTML = wordToGuess;
+document.getElementById('count').innerHTML = countdown;
+document.getElementById('remaining').innerHTML = remainingGuess;
+document.getElementById('grade').innerHTML = score;
+
+}
+
+
